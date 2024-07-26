f7f60fd90de9aa14987de503e11cfc6ccaaf03a9
PauloKinjo <paulo@perfectstorm.gg>
2024-07-26 12:42:10 -0400

    Add SimpleTime class with constructor and BuildString method. Demonstrates time formatting in universal format with aligned output.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+public class ThisTest
+{
+    public static void Main(string[] args)
+    {
+        SimpleTime time = new SimpleTime(15, 30, 19);
+        Console.WriteLine(time.BuildString());
+    }
+}
+
+public class SimpleTime
+{
+    private int hour;
+    private int minute;
+    private int second;
+
+    public SimpleTime(int hour, int minute, int second)
+    {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public string BuildString()
+    {
+        return string.Format("{0,24}: {1}\n{2,24}: {3}",
+           "this.ToUniversalString()", this.ToUniversalString(),
+            "ToUniversalString()", ToUniversalString());
+    }
+
+    private string ToUniversalString()
+    {
+        return string.Format($"{this.hour:D2}:{this.minute:D2}:{this.second}");
+    }
+}
